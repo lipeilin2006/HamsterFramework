9df07a9cf02e002b8976444f8a6bce5f81c6860f
lipeilin2006 <2953079330@qq.com>
2023-04-08 12:23:15 +0800

    update

---
+++ Hamster.Core/Hamster.Core/Hamster.Core.cs
@@ -0,0 +1,219 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Hamster.Core
+{
+	public class HttpServer
+	{
+		HttpListener? listener = null;
+		public string host { get; private set; } = "*";
+		public bool useHttps { get; set; } = false;
+		public byte threadCount { get; private set; } = 1;
+		private Thread[] threads;
+		private Thread? listenLoop;
+		private byte nextThread = 0;
+		private Queue<HttpListenerContext>[] contexts;
+		private bool isExit = false;
+
+		private List<string> routes = new List<string>();
+		private List<Func<HttpListenerRequest, RouteAction>> funcs = new List<Func<HttpListenerRequest, RouteAction>>();
+		private Func<HttpListenerRequest, RouteAction>? other;
+		public HttpServer()
+		{
+			listener = new HttpListener();
+			threads = new Thread[threadCount];
+			contexts=new Queue<HttpListenerContext>[threadCount];
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+		}
+		public HttpServer(string host, bool useHttps,byte threadCount)
+		{
+			this.host = host;
+			this.useHttps = useHttps;
+			this.threadCount = threadCount;
+			listener = new HttpListener();
+			threads = new Thread[threadCount];
+			contexts = new Queue<HttpListenerContext>[threadCount];
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+		}
+
+		public void Start()
+		{
+			try
+			{
+				Console.WriteLine("Starting Server");
+				for (int i = 0; i < contexts.Length; i++)
+				{
+					contexts[i] = new Queue<HttpListenerContext>();
+				}
+				for(int i=0; i<contexts.Length; i++)
+				{
+					object obj = i;
+					ThreadStart start = () =>
+					{
+						int id = (int)obj;
+						while (!isExit)
+						{
+							try
+							{
+								if (contexts[id].Count > 0)
+								{
+									HttpListenerContext context = contexts[id].Dequeue();
+									bool isMatch = false;
+									for(int ii = 0; ii < routes.Count; ii++)
+									{
+										if (Regex.Matches(context.Request.Url.LocalPath, routes[ii]).Count > 0)
+										{
+											isMatch = true;
+											funcs[ii](context.Request).Run(context);
+											break;
+										}
+									}
+									if (!isMatch)
+									{
+										other(context.Request).Run(context);
+									}
+								}
+							}
+							catch (Exception ex)
+							{
+								Console.ForegroundColor = ConsoleColor.Red;
+								Console.WriteLine("[" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "]" + ex.Message.ToString() + " while listening");
+								Console.ForegroundColor = ConsoleColor.White;
+							}
+						}
+					};
+					threads[i] = new Thread(start);
+					threads[i].Start();
+				}
+				listener.Prefixes.Add((useHttps == true ? "https://" : "http://") + host + "/");
+				listener.Start();
+				listenLoop = new Thread(Listen);
+				listenLoop.Start();
+				Console.WriteLine("Listening on " + host);
+			}
+			catch (Exception ex)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("[" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "]" + ex.Message.ToString() + " while starting");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
+		}
+		public void Stop()
+		{
+			try
+			{
+				Console.WriteLine("Stopping server");
+				isExit = true;
+				if (listener != null)
+				{
+					listener.Stop();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message.ToString());
+			}
+		}
+
+		public void Route(string path, Func<HttpListenerRequest, RouteAction> func)
+		{
+			routes.Add(path);
+			funcs.Add(func);
+		}
+		public void RouteOther(Func<HttpListenerRequest, RouteAction> func)
+		{
+			other = func;
+		}
+		private void Listen()
+		{
+			while (!isExit)
+			{
+				try
+				{
+					if (listener != null)
+					{
+						HttpListenerContext context = listener.GetContext();
+						contexts[nextThread].Enqueue(context);
+						Console.WriteLine("[" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "][" + context.Request.RemoteEndPoint + "]" + context.Request.Url.ToString());
+						nextThread++;
+						if (nextThread >= threads.Length)
+						{
+							nextThread = 0;
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine("[" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + "]" + ex.Message.ToString() + " while listening");
+					Console.ForegroundColor = ConsoleColor.White;
+				}
+			}
+		}
+	}
+
+	public abstract class RouteAction
+	{
+		public int statusCode = 200;
+		public abstract void Run(HttpListenerContext context);
+	}
+	public class Text : RouteAction
+	{
+		public string text = "";
+		public string contentType = "";
+		public Text(string text)
+		{
+			this.text = text;
+		}
+		public Text(string text,string contentType)
+		{
+			this.text = text;
+			this.contentType = contentType;
+		}
+
+		public Text(string text, string contentType, int statusCode)
+		{
+			this.text = text;
+			this.contentType = contentType;
+			this.statusCode = statusCode;
+		}
+
+		public override void Run(HttpListenerContext context)
+		{
+			context.Response.AddHeader("Content-type", contentType);
+			context.Response.ContentEncoding = Encoding.UTF8;
+			context.Response.StatusCode = statusCode;
+			context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(text));
+			context.Response.Close();
+		}
+	}
+	public class KFile : RouteAction
+	{
+		public string contentType = "";
+		public byte[] contents;
+		public KFile(string path)
+		{
+			contents = File.ReadAllBytes(path);
+		}
+		public KFile(string path,string contentType)
+		{
+			this.contentType = contentType;
+			contents = File.ReadAllBytes(path);
+		}
+		public KFile(string path, string contentType, int statusCode)
+		{
+			this.contentType = contentType;
+			this.statusCode = statusCode;
+			contents = File.ReadAllBytes(path);
+		}
+
+		public override void Run(HttpListenerContext context)
+		{
+			context.Response.AddHeader("Content-type", contentType);
+			context.Response.StatusCode = statusCode;
+			context.Response.OutputStream.Write(contents);
+			context.Response.Close();
+		}
+	}
+}
---
+++ Hamster/Hamster/Program.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using YamlDotNet.Serialization;
+using System.Reflection;
+using Hamster.Core;
+using System.Net;
+
+string configpath = Path.Combine(".", "config.yaml");
+string scriptpath = "";
+bool isDebug = false;
+if (args.Length > 0)
+{
+	switch (args[0])
+	{
+		case "--start":
+			if (args.Length > 1)
+			{
+				scriptpath = args[1];
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("No C# script was provided");
+				Console.ForegroundColor = ConsoleColor.White;
+				return;
+			}
+			break;
+		case "--debug":
+			if (args.Length > 1)
+			{
+				scriptpath = args[1];
+				isDebug = true;
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("No C# script was provided");
+				Console.ForegroundColor = ConsoleColor.White;
+				return;
+			}
+			break;
+		case "--restart":
+			Environment.SetEnvironmentVariable("KDAction", "restart", EnvironmentVariableTarget.User);
+			return;
+		case "--stop":
+			Environment.SetEnvironmentVariable("KDAction", "stop", EnvironmentVariableTarget.User);
+			return;
+		case "--help":
+			Console.WriteLine("Use --help for help.");
+			Console.WriteLine("--start [C# Script Path] to start server.");
+			Console.WriteLine("--debug [C# Script Path] to start debug server.");
+			Console.WriteLine("--restart to restart server.");
+			Console.WriteLine("--stop to stop server.");
+			return;
+	}
+}
+else
+{
+	Console.WriteLine("Use --help for help.");
+	Console.WriteLine("--start [C# Script Path] to start server.");
+	Console.WriteLine("--debug [C# Script Path] to start debug server.");
+	Console.WriteLine("--restart to restart server.");
+	Console.WriteLine("--stop to stop server.");
+	return;
+}
+Start:
+try
+{
+	List<Assembly> assemblies = new List<Assembly>();
+	Server server;
+	if (File.Exists(configpath))
+	{
+		StringReader input = new StringReader(File.ReadAllText(configpath));
+		var deserializer = new DeserializerBuilder().Build();
+		server = deserializer.Deserialize<Server>(input);
+		input.Close();
+	}
+	else
+	{
+		var serializer = new Serializer();
+		TextWriter textWriter = File.CreateText(configpath);
+		server = new Server()
+		{
+			host = "localhost:20000",
+			useHttps = false,
+			threadCount = 4,
+			imports = { "Hamster.Core.dll", "MySql.Data.dll", "Microsoft.Data.Sqlite.dll", "Oracle.ManagedDataAccess.dll", "Microsoft.Data.SqlClient.dll", "Npgsql.dll", "Dapper.dll" },
+			namespaces = { "System", "System.Net", "Hamster.Core", "System.Threading", "MySql.Data.MySqlClient", "Microsoft.Data.Sqlite", "Oracle.ManagedDataAccess.Client", "Microsoft.Data.SqlClient", "Npgsql" }
+		};
+		serializer.Serialize(textWriter, server);
+		textWriter.Close();
+	}
+	foreach(string dllname in server.imports)
+	{
+		assemblies.Add(Assembly.LoadFile(AppDomain.CurrentDomain.BaseDirectory + dllname));
+	}
+	assemblies.Add(typeof(HttpListenerContext).Assembly);
+	Global global = new Global() { server = new HttpServer(server.host, server.useHttps, server.threadCount) };
+	ScriptOptions scriptOptions = ScriptOptions.Default
+		.AddReferences(assemblies.ToArray())
+		.WithImports(server.namespaces.ToArray());
+	ScriptState scriptState;
+	if (File.Exists(scriptpath))
+	{
+		scriptState = CSharpScript.RunAsync(File.ReadAllText(scriptpath), scriptOptions, global).Result;
+	}
+	else
+	{
+		File.WriteAllText(scriptpath, 
+			"httpServer.Route(\"^/\", (HttpListenerRequest) =>\r\n" +
+			"{\r\n\t" +
+			"return new Text(\"Hello\");\r\n" +
+			"});\r\n"+
+			"server.RouteOther((HttpListenerRequest request) =>\r\n" +
+			"{\r\n\t\t" +
+			"return new Text(\"404\");\r\n" +
+			"});");
+		scriptState = CSharpScript.RunAsync(File.ReadAllText(scriptpath), scriptOptions, global).Result;
+	}
+	global.server.Start();
+	if (isDebug)
+	{
+		while (true)
+		{
+			Console.Write("C#>>");
+			string order = Console.ReadLine();
+			if (order == "Stop()")
+			{
+				global.server.Stop();
+				return;
+			}
+			else
+			{
+				try
+				{
+					scriptState = scriptState.ContinueWithAsync(order, scriptOptions).Result;
+				}
+				catch (Exception e)
+				{
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine(e.Message);
+					Console.ForegroundColor = ConsoleColor.White;
+				}
+			}
+		}
+	}
+	else
+	{
+		while (true)
+		{
+			switch (Environment.GetEnvironmentVariable("KDAction", EnvironmentVariableTarget.User))
+			{
+				case "restart":
+					global.server.Stop();
+					goto Start;
+				case "stop":
+					global.server.Stop();
+					return;
+			}
+		}
+	}
+}
+catch (Exception e)
+{
+	Console.ForegroundColor = ConsoleColor.Red;
+	Console.WriteLine(e.Message);
+	Console.ForegroundColor = ConsoleColor.White;
+}
+
+public class Server
+{
+	public string host="localhost:5000";
+	public bool useHttps;
+	public byte threadCount;
+	public HashSet<string> imports = new HashSet<string>();
+	public HashSet<string> namespaces = new HashSet<string>();
+}
+
+public class Global
+{
+	public HttpServer server;
+}
